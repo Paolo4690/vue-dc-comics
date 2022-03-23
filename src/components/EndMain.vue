efe17662b77579e592303286fff547e5550e33c3
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-23 16:02:34 +0100

    add more components

---
+++ src/components/EndMain.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="end-main">
+    <div class="container">
+      <div v-for="(card, index) in arrSectionBlu" :key="index" class="content-card">
+        <img :src="card.img" :alt="card.text">
+        <span>{{ card.text }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card1 from '../assets/img/buy-comics-digital-comics.png'
+import card2 from '../assets/img/buy-comics-merchandise.png'
+import card3 from '../assets/img/buy-comics-subscriptions.png'
+import card4 from '../assets/img/buy-comics-shop-locator.png'
+import card5 from '../assets/img/buy-dc-power-visa.svg'
+
+export default {
+  name: 'EndMain',
+  data () {
+    return {
+      arrSectionBlu: [
+        {
+          img: card1,
+          text: 'digitals comics'
+        },
+        {
+          img: card2,
+          text: 'dc merchandise'
+        },
+        {
+          img: card3,
+          text: 'subscription'
+        },
+        {
+          img: card4,
+          text: 'comic shop locator'
+        },
+        {
+          img: card5,
+          text: 'dc power visa'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/partial/variables';
+.end-main {
+  display: flex;
+  height: 100px;
+  background-color: $blu_color;
+  font-size: 1.3rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  .container {
+    flex: 1 0 100%;
+    justify-content: space-between;
+    .content-card {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: calc(100% / 5);
+      img {
+        width: 40px;
+      }
+      span {
+        padding-left: .3rem;
+        font-size: .8rem;
+        font-weight: normal;
+      }
+    }
+  }
+}
+</style>
--- src/components/MainComics.vue
+++ src/components/MainComics.vue
@@ -1,218 +1,26 @@
 <template>
   <main>
     <JumboMain />
-    <div class="top-main">
-      <div class="container">
-        <div class="btn-blu-before">current series</div>
-        <div class="container-books">
-          <CardMain  v-for="item in arrBooks"
-            :key="item.series"
-            :book-img="item.thumb"
-            :book-title="item.series"
-          />
-        </div>
-        <div class="btn-blu-after">Load More</div>
-      </div>
-    </div>
-    <div class="end-main">
-      <div class="container">
-        <div v-for="(card, index) in arrSectionBlu" :key="index" class="content-card">
-          <img :src="card.img" :alt="card.text">
-          <span>{{ card.text }}</span>
-        </div>
-      </div>
-    </div>
+    <TopMain />
+    <EndMain />
   </main>
 </template>
 
 <script>
 import JumboMain from './JumboMain.vue'
-import CardMain from './CardMain.vue'
-
-import card1 from '../assets/img/buy-comics-digital-comics.png'
-import card2 from '../assets/img/buy-comics-merchandise.png'
-import card3 from '../assets/img/buy-comics-subscriptions.png'
-import card4 from '../assets/img/buy-comics-shop-locator.png'
-import card5 from '../assets/img/buy-dc-power-visa.svg'
+import TopMain from './TopMain.vue'
+import EndMain from './EndMain.vue'
 
 export default {
   name: 'MainComics',
   components: {
     JumboMain,
-    CardMain
-  },
-  data () {
-    return {
-      arrBooks: [
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX',
-          price: '$19.99',
-          series: 'Action Comics',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01',
-          price: '$3.99',
-          series: 'American Vampire 1976',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb',
-          price: '$16.99',
-          series: 'Aquaman',
-          type: 'graphic novel'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76',
-          price: '$2.99',
-          series: 'Batgirl',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
-          price: '$3.99',
-          series: 'Batman',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ',
-          price: '$2.99',
-          series: 'Batman Beyond',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8',
-          price: '$3.99',
-          series: 'Batman/Superman',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk',
-          price: '$4.99',
-          series: 'Batman/Superman Annual',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8',
-          price: '$5.99',
-          series: 'Batman: The Joker War Zone',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY',
-          price: '$6.99',
-          series: 'Batman: Three Jokers',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX',
-          price: '$4.99',
-          series: 'Batman: White Knight Presents: Harley Quinn',
-          type: 'comic book'
-        },
-        {
-          thumb: 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq',
-          price: '$16.99',
-          series: 'Catwoman',
-          type: 'graphic novel'
-        }
-      ],
-      arrSectionBlu: [
-        {
-          img: card1,
-          text: 'digitals comics'
-        },
-        {
-          img: card2,
-          text: 'dc merchandise'
-        },
-        {
-          img: card3,
-          text: 'subscription'
-        },
-        {
-          img: card4,
-          text: 'comic shop locator'
-        },
-        {
-          img: card5,
-          text: 'dc power visa'
-        }
-      ]
-    }
+    TopMain,
+    EndMain
   }
 }
 </script>
 
-<style scoped lang="scss">
-@import '../assets/styles/partial/variables';
-.top-main {
-  display: flex;
-  background-color: #1c1c1c;
-  font-size: 1.3rem;
-  font-weight: bold;
-  padding-bottom: 1rem;
-  .container {
-    position: relative;
-    flex-wrap: wrap;
-    justify-content: center;
-    .btn-blu-before {
-      position: absolute;
-      top: -18px;
-      left: 0;
-      width: 160px;
-      height: 35px;
-      background-color: $blu_color;
-      line-height: 35px;
-      text-align: center;
-      font-size: 1rem;
-      font-weight: 500;
-      text-transform: uppercase;
-      cursor: pointer;
-    }
-    .container-books {
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      padding: 2rem 0 1rem;
-    }
-    .btn-blu-after {
-      width: 160px;
-      height: 35px;
-      background-color: $blu_color;
-      line-height: 35px;
-      text-align: center;
-      font-size: .9rem;
-      font-weight: 400;
-      text-transform: uppercase;
-      cursor: pointer;
-    }
-  }
-}
-.end-main {
-  display: flex;
-  height: 100px;
-  background-color: $blu_color;
-  font-size: 1.3rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  .container {
-    flex: 1 0 100%;
-    justify-content: space-between;
-    .content-card {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: calc(100% / 5);
-      img {
-        width: 40px;
-      }
-      span {
-        padding-left: .3rem;
-        font-size: .8rem;
-        font-weight: normal;
-      }
-    }
-  }
-}
+<style>
+
 </style>
